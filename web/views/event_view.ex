d71142949ad27350326e2229d611183ae8a32588
gerCasas <gcasasg4@gmail.com>
2017-03-15 17:02:51 -0700

    Event_view, array of event details filtered by different nil

--- web/views/event_view.ex
+++ web/views/event_view.ex
@@ -14,6 +14,24 @@ defmodule BoleteraApi.EventView do
     # IO.inspect(event)
 
     if Map.has_key?(event, :city_name) do
+
+      event_details = %{
+        event_details: [
+          %{key: "Refounds", value: event.refounds},
+          %{key: "Gifting", value: event.gifting},
+          %{key: "Assistance", value: event.assistance},
+          %{key: "Prices", value: event.prices},
+          %{key: "Restrictions", value: event.restrictions},
+          %{key: "Tickets", value: event.tickets},
+          %{key: "Questions", value: event.questions},
+          %{key: "Age Restrictions", value: event.age_restrictions},
+          %{key: "Purchase Limit", value: event.purchase_limit},
+          %{key: "Additional Info", value: event.additional_info}
+        ]
+      }
+
+      event_details = filter_null_event_keys(event_details)
+
       %{id: event.id,
         name: event.name,
         description: event.description,
@@ -30,19 +48,8 @@ defmodule BoleteraApi.EventView do
         city_id: event.city_id,
         city_name: event.city_name,
 
-        details: %{
-            refounds: event.refounds,
-            gifting: event.gifting,
-            assistance: event.assistance,
-            prices: event.prices,
-            restrictions: event.restrictions,
-            tickets: event.tickets,
-            questions: event.questions,
-            age_restrictions: event.age_restrictions,
-            purchase_limit: event.purchase_limit,
-            additional_info: event.additional_info
-          }
-        }
+        details: event_details
+      }
 
     else
       %{id: event.id,
@@ -70,6 +77,18 @@ defmodule BoleteraApi.EventView do
         video_url: event.video_url
         }
     end
-
   end
+
+  def filter_null_event_keys(event) do
+    eventfiltered = Enum.filter(event.event_details, &filter_nulls/1)
+  end
+
+  def filter_nulls(row) do
+    if row.value != nil do
+      row
+    else
+      false
+    end
+  end
+
 end
